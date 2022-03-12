930ab21befa12149ba3b677b3409c903443b787f
Riley Bergamasco <rlbergamasco@gmail.com>
2022-03-12 15:46:15 -0500

    Created theme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Typography, createTheme } from '@mui/material';
+import { Box, createTheme, ThemeProvider } from '@mui/material';
 import { HomePage, GraphicsPage } from 'pages';
 
 const ROUTES = [
@@ -20,24 +20,72 @@ const ROUTES = [
 function App() {
   const theme = createTheme({
     palette: {
-      primary: {
-        main: '#F96C5F',
-      },
+      text: {
+        primary: "#FFFFFF",
+        secondary: "#000000"
+      }
     },
     typography: {
+      fontFamily: [
+        'Lato',
+        'Roboto',
+        'Helvetica',
+        'Arial',
+        'sans-serif'
+      ].join(','),
+      h1: {
+        fontFamily: [
+          'Montserrat',
+          'Roboto',
+          'Helvetica',
+          'Arial',
+          'sans-serif'
+        ].join(','),
+        fontWeight: 700,
+        fontSize: '4.2rem',
+      },
+      h2: {
+        fontFamily: [
+          'Montserrat',
+          'Roboto',
+          'Helvetica',
+          'Arial',
+          'sans-serif'
+        ].join(','),
+        fontWeight: 700,
+        fontSize: '2.4rem',
+      },
+      h3: {
+        fontFamily: [
+          'Montserrat',
+          'Roboto',
+          'Helvetica',
+          'Arial',
+          'sans-serif'
+        ].join(','),
+        fontWeight: 500,
+        fontSize: '1.5rem',
+      },
+      body1: {
+        fontSize: '1.2rem',
+      }
     }
   });
 
   return (
-    <Routes>
-      {ROUTES.map((route) => (
-        <Route
-          key={route.path}
-          exact={route.path === '/'}
-          {...route}
-        />
-      ))}
-    </Routes>
+    <ThemeProvider theme={theme}>
+      <Box sx={{ height: "100vh", width: "100vw", backgroundColor: "#1C1C1C" }}>
+        <Routes>
+          {ROUTES.map((route) => (
+            <Route
+              key={route.path}
+              exact={route.path === '/'}
+              {...route}
+            />
+          ))}
+        </Routes>
+      </Box>
+    </ThemeProvider>
   );
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,30 @@
-import { Typography } from '@mui/material';
+import React from 'react';
+import { Box, Typography } from '@mui/material';
 
 const HomePage = () => {
     return (
-        <Typography>Home</Typography>
+        <Box sx={{ p: 5 }}>
+            <AboutSection />
+        </Box>
     )
 };
+
+const AboutSection = () => {
+    return (
+        <Box sx={{ maxWidth: '660px' }}>
+            <Typography color="textPrimary" variant="h1">About Me</Typography>
+            <Typography color="textPrimary">
+                {"My name is Riley Bergamasco. I'm a junior in the Hussman School of Journalism and Media at the University of North Carolina at Chapel Hill double majoring in Media and Journalism (Interactive Media Concentration) & Computer Science with a minor in Cognitive Science."}
+            </Typography>
+            <Typography color="textPrimary">
+                I have experience with UI/UX design, web and mobile app development, AR development, and more. I also have experience designing logos, branding, magazine layouts, illustrations, infographics, websites, and motion graphics.
+            </Typography>
+            <Typography color="textPrimary">
+                Learn more about my experience, skills, and interests by downloading my resume, connecting with me on LinkedIn, or checking out my Github.            </Typography>
+            <Typography color="textPrimary">
+                I designed and developed this website and all of its content. This website was built using React.            </Typography>
+        </Box>
+    )
+}
 
 export { HomePage };
