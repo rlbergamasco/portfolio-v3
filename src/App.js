e8f44dca0ed7671df499b76ff4e031f50190cf73
Riley Bergamasco <rlbergamasco@gmail.com>
2022-03-12 15:06:16 -0500

    Set up routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import { Typography } from '@mui/material';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Typography, createTheme } from '@mui/material';
+import { HomePage, GraphicsPage } from 'pages';
+
+const ROUTES = [
+  {
+    label: 'Home',
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    label: 'Graphics',
+    path: '/graphics',
+    element: <GraphicsPage />,
+  },
+];
+
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#F96C5F',
+      },
+    },
+    typography: {
+    }
+  });
+
   return (
-    <div>
-      <Typography>Riley Bergamasco</Typography>
-    </div>
+    <Routes>
+      {ROUTES.map((route) => (
+        <Route
+          key={route.path}
+          exact={route.path === '/'}
+          {...route}
+        />
+      ))}
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { CssBaseline } from '@mui/material';
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <CssBaseline />
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
